8f23c8b0573804ac06829bc6fa3c659562fe258d
Jongmin Kim <kimjmin@gmail.com>
2014-07-03 16:27:12 +0900

    init commit.

---
+++ MyPickCurrRate/src/curr/CurrRate.java
@@ -0,0 +1,10 @@
+package curr;
+
+public class CurrRate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GetCurrRate opt = new GetCurrRate();
+		opt.getInfo();
+	}
+}
